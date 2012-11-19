71c38f2d5826692f3f84f76fc7d03cfd01121d06
unknown <umang@umang-PC.(none)>
2012-11-19 20:09:04 +0530

    NoSuchRecordException.java file

---
+++ src/java/in/umang/NoSuchRecordException.java
@@ -0,0 +1,14 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package in.umang;
+
+/**
+ *
+ * @author umang
+ */
+public class NoSuchRecordException extends Exception
+{
+    
+}
