5a20ab389c599f6aff53fefd9d494d3e3391958c
unknown <umang@umang-PC.(none)>
2012-11-19 17:13:21 +0530

    created database.java fil

---
+++ src/java/in/umang/Database.java
@@ -0,0 +1,33 @@
+
+package in.umang;
+
+import java.sql.*;
+
+/**
+ *
+ * @author umang
+ */
+public class Database
+{
+
+    public static Connection getConnection()
+    {
+        Connection aConnection = null;
+        try
+        {
+            Class.forName("com.mysql.jdbc.Driver");
+            aConnection = DriverManager.getConnection("jdbc:mysql://localhost:3306/WDAJ", "root", "");
+            if(aConnection==null)
+            {
+                throw new Exception();
+            }
+        }
+        catch (Exception e)
+        {
+            RuntimeException aRuntimeException = new RuntimeException("Could not load connection");
+            aRuntimeException.setStackTrace(e.getStackTrace());
+        }
+        return aConnection;
+
+    }
+}
